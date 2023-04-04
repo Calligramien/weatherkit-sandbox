d7937886b4af722b4bc7f0c146af9866015b2e26
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-04-04 17:53:13 +0200

    Initial Commit

---
+++ Weatherkit Sandbox/Weatherkit_SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weatherkit_SandboxApp.swift
+//  Weatherkit Sandbox
+//
+//  Created by Damien Gautier on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weatherkit_SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
